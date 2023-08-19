4a0022299aa41b9d34a63407d90e916abf22e57b
Billa008 <136236635+Billa008@users.noreply.github.com>
2023-08-19 22:45:20 +0530

    Update group_filter.py

--- plugins/group_filter.py
+++ plugins/group_filter.py
@@ -115,7 +115,8 @@ async def next_page(bot, query):
     else:
         off_set = offset - 10
     if n_offset == 0:
-        [InlineKeyboardButton("⏪ BACK", callback_data=f"next_{req}_{key}_{off_set}"),
+        btn.append(
+            [InlineKeyboardButton("⏪ BACK", callback_data=f"next_{req}_{key}_{off_set}"),
              InlineKeyboardButton(f"📃 Pages {round(int(offset) / 10) + 1} / {round(total / 10)}",
                                   callback_data="pages")]
         )
